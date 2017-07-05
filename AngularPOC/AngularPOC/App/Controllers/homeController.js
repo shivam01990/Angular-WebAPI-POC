59930ea173dace582b3efc74b6ded2d035c7c376
Shivam Srivastava <shivam01990@gmail.com>
2017-07-05 16:52:56 +0530

    home controller

--- AngularPOC/AngularPOC/App/Controllers/homeController.js
+++ AngularPOC/AngularPOC/App/Controllers/homeController.js
@@ -1,11 +1,13 @@
-﻿app.controller("Page1Controller", function ($scope, $location, HomeService) {
+debugger;
+app.controller("Page1Controller", function ($scope, $location, HomeService) {
     getContent();
 
-    function getContent() {
+	function getContent() {
+		debugger;
         var promiseobj = HomeService.GetDataPage1();
 
         promiseobj.then(function (pl) {
-            $scope.content = pl;
+            $scope.content = pl.data;
         },
             function (errorPl) {
                 $scope.error = 'failure loading', errorPl;
@@ -20,7 +22,7 @@ app.controller("Page2Controller", function ($scope, $location, HomeService) {
         var promiseobj = HomeService.GetDataPage2();
 
         promiseobj.then(function (pl) {
-            $scope.content = pl;
+            $scope.content = pl.data;
         },
             function (errorPl) {
                 $scope.error = 'failure loading', errorPl;
@@ -35,7 +37,7 @@ app.controller("Page3Controller", function ($scope, $location, HomeService) {
         var promiseobj = HomeService.GetDataPage3();
 
         promiseobj.then(function (pl) {
-            $scope.content = pl;
+            $scope.content = pl.data;
         },
             function (errorPl) {
                 $scope.error = 'failure loading', errorPl;
