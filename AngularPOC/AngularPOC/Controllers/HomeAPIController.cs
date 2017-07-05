27d5deae97ddea583046c748e8d88312a1abd022
Shivam Srivastava <shivam01990@gmail.com>
2017-07-05 23:42:26 +0530

    csproj changes

--- AngularPOC/AngularPOC/Controllers/HomeAPIController.cs
+++ AngularPOC/AngularPOC/Controllers/HomeAPIController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace AngularPOC.Controllers
 {
